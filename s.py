a5099f77f20307eeabe4e6d442e7c17738955783
Mr insane <108201436+insanepk@users.noreply.github.com>
2022-06-26 05:41:43 +0500

    s.py
    It's work in 32bit😂😁💥
    
    pkg update
    
    pkg upgrade
    
    pkg install git
    
    pkg install python
    
    pkg install python2
    
    pip2 install requests
    
    pip2 install requirements
    
    pip2 install mechanize
    
    pip install rich
    
    pip install bs4
    
    rm -rf SSB
    
    git clone https://github.com/insanepk/SSB.git
    
    cd SSB
    
    python s.py

---
+++ s.py
@@ -0,0 +1,17 @@
+import os, platform, time
+
+b = platform.architecture()[0]
+
+if b == '64bit':
+
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    os.system('xdg-open https://youtu.be/3CVApc0CkhY/');time.sleep(5)
+
+    import SSB
+
+elif b == '32bit':
+
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    os.system('xdg-open xdg-open https://api.whatsapp.com/send/?phone=923086271330&text=Hello_mr_insane/');time.sleep(5)
